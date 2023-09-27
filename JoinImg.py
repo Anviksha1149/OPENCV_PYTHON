680cb5b49a16f6795c498b087743b6c712cd23f9
Anviksha1149 <145531505+Anviksha1149@users.noreply.github.com>
2023-09-27 23:59:41 +0530

    Update JoinImg.py

--- JoinImg.py
+++ JoinImg.py
@@ -4,6 +4,7 @@ import numpy as np
 img=cv2.imread("Resources/images/img2.jpg")
 
 imgHor=np.hstack((img,img))
-
+imgVer=np.vstack((img,img))
 cv2.imshow("Output",imgHor)
+cv2.imshow("output2",imgVer)
 cv2.waitKey(0)
