a6d5223f6e1b67e7ee0bac4a8e821b4fb51ba738
Anviksha1149 <145531505+Anviksha1149@users.noreply.github.com>
2023-09-27 18:18:37 +0530

    Create ImportVideo.py

---
+++ ImportVideo.py
@@ -0,0 +1,7 @@
+import cv2
+cap=cv2.VideoCapture("Resources/Videos/cat.mp4")
+while True:
+    success,img=cap.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1) & 0xff==ord('q'):
+        break
